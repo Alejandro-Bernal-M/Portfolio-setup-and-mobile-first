07614f7673e668ac464f1e6f8bda4688df3c2d5c
alejandro <alejandrober96@gmail.com>
2023-01-31 10:22:43 -0500

    Linterns corrections

--- java.js
+++ java.js
@@ -1,15 +1,21 @@
-const menu = document.querySelector("#hambur");
-const modal = document.querySelector(".modal-menu");
-const modalImg = document.querySelector(".modal-img");
-const modalA1 = document.querySelector(".modal-a1");
-const modalA2 = document.querySelector(".modal-a2");
-const modalA3 = document.querySelector(".modal-a3");
-menu.addEventListener("click", () => {
-  modal.classList.add("modal-show");
+const menu = document.querySelector('#hambur');
+const modal = document.querySelector('.modal-menu');
+const modalImg = document.querySelector('.modal-img');
+const modalA1 = document.querySelector('.modal-a1');
+const modalA2 = document.querySelector('.modal-a2');
+const modalA3 = document.querySelector('.modal-a3');
+menu.addEventListener('click', () => {
+  modal.classList.add('modal-show');
 });
-modalImg.addEventListener("click", () => {
-  modal.classList.remove("modal-show");
+modalImg.addEventListener('click', () => {
+  modal.classList.remove('modal-show');
 });
-modalA1.addEventListener("click", () => {
-  modal.classList.remove("modal-show");
+modalA1.addEventListener('click', () => {
+  modal.classList.remove('modal-show');
+});
+modalA2.addEventListener('click', () => {
+  modal.classList.remove('modal-show');
+});
+modalA3.addEventListener('click', () => {
+  modal.classList.remove('modal-show');
 });
