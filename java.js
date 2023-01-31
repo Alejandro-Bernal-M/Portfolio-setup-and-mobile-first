b9a54669862982426d7d07bd68577550690b3947
cabraham1 <christopherabraham8@gmail.com>
2023-01-31 15:39:53 +0100

    Implementation of javascript menu

---
+++ java.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector("#hambur");
+const modal = document.querySelector(".modal-menu");
+const modalImg = document.querySelector(".modal-img");
+const modalA1 = document.querySelector(".modal-a1");
+const modalA2 = document.querySelector(".modal-a2");
+const modalA3 = document.querySelector(".modal-a3");
+menu.addEventListener("click", () => {
+  modal.classList.add("modal-show");
+});
+modalImg.addEventListener("click", () => {
+  modal.classList.remove("modal-show");
+});
+modalA1.addEventListener("click", () => {
+  modal.classList.remove("modal-show");
+});
